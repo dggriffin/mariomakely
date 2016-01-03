64c9eb58e33ce1fd58b45716066629c495672344
dggriffin <donggriffin@gmail.com>
2016-01-02 23:11:01 -0500

    Add undefine check on search level helper.

--- client/templates/search/search.js
+++ client/templates/search/search.js
@@ -4,36 +4,39 @@
         levels: function() {
             searchString.depend();
             var value = $('input').val();
+            var levels;
             // var tag = Session.get('tags');
             // value = value ? value : tag;
-            var levels = Levels.find({
-                tags: {
-                    $in: [value.toLowerCase()]
-                }
-            }, {
-                sort: {
-                    date: -1
-                }
-            }).fetch();
+            if (value) {
+                levels = Levels.find({
+                    tags: {
+                        $in: [value.toLowerCase()]
+                    }
+                }, {
+                    sort: {
+                        date: -1
+                    }
+                }).fetch();
 
-            levels.forEach((level) => {
-                var postTime = new Date(level.date);
-                var currentTime = new Date();
+                levels.forEach((level) => {
+                    var postTime = new Date(level.date);
+                    var currentTime = new Date();
 
-                var timeDiff = currentTime - postTime;
-                var msec = timeDiff;
-                var dd = Math.floor(msec / 1000 / 60 / 60 / 24);
-                var hh = Math.floor(msec / 1000 / 60 / 60);
-                msec -= hh * 1000 * 60 * 60;
-                var mm = Math.floor(msec / 1000 / 60);
-                if (dd) {
-                    level.date = dd + " days ago"
-                } else {
-                    level.date = hh ? hh + " hours and " + mm + " minutes ago" : mm + " minutes ago";
-                }
-            });
+                    var timeDiff = currentTime - postTime;
+                    var msec = timeDiff;
+                    var dd = Math.floor(msec / 1000 / 60 / 60 / 24);
+                    var hh = Math.floor(msec / 1000 / 60 / 60);
+                    msec -= hh * 1000 * 60 * 60;
+                    var mm = Math.floor(msec / 1000 / 60);
+                    if (dd) {
+                        level.date = dd + " days ago"
+                    } else {
+                        level.date = hh ? hh + " hours and " + mm + " minutes ago" : mm + " minutes ago";
+                    }
+                });
 
-            return levels;
+                return levels;
+            }
         },
         levelCount: function() {
             searchString.depend();
